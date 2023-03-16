a362da6325a01919a63ab8a6e3ba367846bfacb3
ZukSkyWalker <wzukai@gmail.com>
2023-03-16 11:44:51 -0700

    Correcting the pow backward

--- minigrad.py
+++ minigrad.py
@@ -10,10 +10,9 @@ class Value:
     self.label = label
   
   def __repr__(self):
-    return f"Value(data={self.data:.2f}, grad={self.grad:.2f}), operation={self._op}"
+    return f"Value(data={self.data:.2f}, grad={self.grad:.2f})"
 
   def __add__(self, other):
-    # So you can add a number to self
     other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data + other.data, (self, other), '+')
     def _backward():
@@ -35,11 +34,11 @@ class Value:
     return out
   
   def __pow__(self, other):
-    assert isinstance(other, (int, float)), "only supporting int/float powers"
-    out = Value(self.data**other, (self,), f'^{other}')
+    assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+    out = Value(self.data**other, (self,), f'**{other}')
     
     def _backward():
-      self.grad += other * out.data / self.data * out.grad
+      self.grad += other * (self.data**(other-1)) * out.grad
     
     out._backward = _backward
     
@@ -54,7 +53,7 @@ class Value:
   def __neg__(self): # -self
     return self * -1
 
-  def __sub__(self, other):
+  def __sub__(self, other): # self - other
     return self + (-other)
 
   def __radd__(self, other): # other + self
@@ -66,16 +65,16 @@ class Value:
   def __rtruediv__(self, other): # other / self
     return other * self**-1
 
+
   def tanh(self):
     x = self.data
     t = (np.exp(2*x) - 1) / (np.exp(2*x) + 1)
-    out = Value(data=t, _children = (self, ), _op='tanh')
+    out = Value(t, (self, ), 'tanh')
     
     def _backward():
-      self.grad += (1-t**2) * out.grad
+      self.grad += (1 - t**2) * out.grad
     
-    out._backward = _backward 
-    
+    out._backward = _backward
     return out
   
   def exp(self):
@@ -87,7 +86,6 @@ class Value:
     out._backward = _backward
     return out
 
-
   def backward(self):
     # Do the backpropagation in reversed order
     topo = []
@@ -97,7 +95,6 @@ class Value:
       if v not in visited:
         visited.add(v)
 
-        # Append you to the topo list only after all your children are appended
         for child in v._prev:
           build_topo(child)
         topo.append(v)
@@ -105,7 +102,7 @@ class Value:
     build_topo(self)
 
     self.grad = 1.0
-
     for node in reversed(topo):
       # print(f"Backward propagate:", node)
       node._backward()
+
